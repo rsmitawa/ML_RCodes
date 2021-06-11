5ffe8b81ac2d58cb9cb39d15ba1cf9aeb390f399
rsmitawa <rsmitawa@gmail.com>
2021-06-10 20:07:38 -0400

    Comparing two dataframes

--- R-Hacks.R
+++ R-Hacks.R
@@ -1,2 +1,13 @@
 # Read zip file directy in R using fread
 main_df <- as.data.frame(fread(input = "unzip -p operations.csv.zip", stringsAsFactors = TRUE, nThread = 8))
+
+# compare two dataframes, if they are same or identical
+
+base::table(df1 == df2, useNA = "ifany")
+# True count, False count, and null count
+
+base::identical(df1, df2)
+# TRUE, FALSE
+
+base::all.equal(df1, df2)
+# True or mismatch column name
